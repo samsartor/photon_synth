28c16853f129344825569113bab22d757c4932f1
Sam Sartor <me@samsartor.com>
2018-12-03 21:06:08 -0700

    frequency modulation

--- asynth.ino
+++ asynth.ino
@@ -2,12 +2,14 @@ const int NOTE_COUNT = 4;
 const float NOTES[NOTE_COUNT] = {261.63, 293.66, 329.63, 392.00};
 const int BUTTONS[NOTE_COUNT] = {D0, D1, D2, D3};
 const int AUDIO_OUTPUT = A4;
+const int MOD_INTPUT = A0;
 
 void setup() {
     for (int i=0; i < NOTE_COUNT; i++) {
         pinMode(BUTTONS[i], INPUT_PULLUP);
     }
     pinMode(AUDIO_OUTPUT, OUTPUT);
+    pinMode(MOD_INTPUT, INPUT);
 }
 
 static int state = -1;
@@ -21,14 +23,14 @@ void loop() {
         }
     }
 
-    if (state != new_state) {
-        state = new_state;
-        if (state == -1) {
-            analogWrite(AUDIO_OUTPUT, 0);
-        } else {
-            analogWrite(AUDIO_OUTPUT, 128, NOTES[state]);
-        }
+    state = new_state;
+    if (state == -1) {
+        analogWrite(AUDIO_OUTPUT, 0);
+    } else {
+        float mult = analogRead(MOD_INTPUT) / 4096.0 + 1.0;
+        float freq = NOTES[state] * mult;
+        analogWrite(AUDIO_OUTPUT, 128, freq);
     }
 
-    delay(10);
+    delay(100);
 }
