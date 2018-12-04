03987f96ec685a46dc04e5d6f05adc57ff6d4b33
Sam Sartor <me@samsartor.com>
2018-12-03 20:48:22 -0700

    different PWM frequencies

--- asynth.ino
+++ asynth.ino
@@ -1,26 +1,35 @@
-const float NOTES[4] = {261.63, 293.66, 329.63, 392.00};
-const int BUTTONS[4] = {D0, D1, D2, D3};
-const int LED = D7;
+const int NOTE_COUNT = 4;
+const float NOTES[NOTE_COUNT] = {261.63, 293.66, 329.63, 392.00};
+const int BUTTONS[NOTE_COUNT] = {D0, D1, D2, D3};
+const int AUDIO_OUTPUT = A4;
 
 void setup() {
-  pinMode(D0), INPUT_PULLUP);
-  pinMode(LED, OUTPUT);
-  pinMode(A4, OUTPUT);
-  pinMode(DAC1, OUTPUT);
-  analogWrite(A4, 128, 1000);
+    for (int i=0; i < NOTE_COUNT; i++) {
+        pinMode(BUTTONS[i], INPUT_PULLUP);
+    }
+    pinMode(AUDIO_OUTPUT, OUTPUT);
+    pinMode(LED, OUTPUT);
 }
 
+static int state = -1;
+
 void loop() {
-//    int period = 1e6 / NOTES[0];
-//    long cycle = micros() % period;
-//    int out;
-//    if (cycle > (period / 2)) {
-//        out = 4095;
-//    } else {
-//        out = 0;
-//    }
-//
-//    if (digitalRead(BUTTONS[0]) == LOW) {
-//        analogWrite(A4, 128, 1000);
-//    }
+    int new_state = -1;
+    for (int i=0; i < NOTE_COUNT; i++) {
+        if (digitalRead(BUTTONS[i]) == LOW) {
+            new_state = i;
+            break;
+        }
+    }
+
+    if (state != new_state) {
+        state = new_state;
+        if (state == -1) {
+            analogWrite(AUDIO_OUTPUT, 0);
+        } else {
+            analogWrite(AUDIO_OUTPUT, 128, NOTES[state]);
+        }
+    }
+
+    delay(10);
 }
